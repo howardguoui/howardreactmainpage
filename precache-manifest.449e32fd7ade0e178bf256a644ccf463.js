441698ad1b0d39b122e52a4ff6f1b4107a1fcb65
Howard <howardguoui@gmail.com>
2020-09-02 10:17:49 -0400

    Updates

---
+++ precache-manifest.449e32fd7ade0e178bf256a644ccf463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891620a7c4d571302bfdb6c81ef2d08e",
+    "url": "/howardreactmainpage/index.html"
+  },
+  {
+    "revision": "9940175c5f49665db0ce",
+    "url": "/howardreactmainpage/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a5080e096ca6dbf8c77e",
+    "url": "/howardreactmainpage/static/js/2.b2a795d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/howardreactmainpage/static/js/2.b2a795d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9940175c5f49665db0ce",
+    "url": "/howardreactmainpage/static/js/main.561c679f.chunk.js"
+  },
+  {
+    "revision": "265bcd2088a8abf72553",
+    "url": "/howardreactmainpage/static/js/runtime-main.30999467.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.665c915afedba1e1f3061f4e457bfd45.js"
+  "/howardreactmainpage/precache-manifest.449e32fd7ade0e178bf256a644ccf463.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/howardreactmainpage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
